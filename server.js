798d9eb4ea917e477545ad921f09f5ebb4c84cfa
hassenmrakbenjebahi <123837615+hassenmrakbenjebahi@users.noreply.github.com>
2023-11-04 15:53:26 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = express();
+const port = process.env.PORT || 9090;
+const databaseName = 'ecoshopdb';
+const db_url = process.env.DB_URL || `mongodb://localhost:27017`;
+
+mongoose
+  .connect(`${db_url}/${databaseName}`)
+  .then(() => {
+    console.log(`Connected to ${databaseName}`);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
+  
