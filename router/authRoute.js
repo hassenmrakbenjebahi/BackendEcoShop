80a4755737e540592103d56573c1bd63e90c3050
bilelyousfi <yousfi.bilel@esprit.tn>
2023-11-13 21:19:47 +0100

    firstcommit

---
+++ router/authRoute.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import * as authController from '../controllers/authcontroller.js';
+const router = express.Router();
+import multer from '../middlewares/multer-config.js'
+ 
+router.route("/signup").post(multer,authController.signup);
+router.route("/login").post(authController.login);
+router.route("/users").get(authController.protect ,authController.getUsers);
+router.route("/otp").post(authController.otp);
+
+router.route("/forgetPassword").post(authController.forgetPassword);
+router.route("/resetPassword").patch(authController.resetPassword);
+
+
+
+export default router;  
--- server.js
+++ server.js
@@ -1,18 +1,38 @@
-const app = express();
-const port = process.env.PORT || 9090;
-const databaseName = 'ecoshopdb';
-const db_url = process.env.DB_URL || `mongodb://localhost:27017`;
+import express from 'express';
 
+import { notFoundError } from './middlewares/error.js';
+import mongoose from 'mongoose';
+
+import morgan from 'morgan';
+
+import authRouter from './router/authRoute.js'
+
+const app = express()
+const hostanme = "127.0.0.1"
+const port = 3000
+
+// app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.use(express.urlencoded({ extended: true }))
+//DB
+mongoose.set('debug', true)
+mongoose.Promise = global.Promise;
 mongoose
-  .connect(`${db_url}/${databaseName}`)
-  .then(() => {
-    console.log(`Connected to ${databaseName}`);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .connect('mongodb://127.0.0.1:27017/User')
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => {
+        console.log(err);
+    })
 
-  app.listen(port, () => {
+app.use('/user',authRouter)
+app.use(express.urlencoded({ extended: true }));
+app.use(notFoundError)
+
+
+
+app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
-  });
-  
+});
+
